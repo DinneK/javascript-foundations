5cbc51817557e90a7fcfa8dccc60a9fa74e3e20f
Dinne Kopelevich <dinne22@hotmail.com>
2022-05-18 16:50:17 -0600

    Finish unicorn and dragon

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,27 @@
 class Unicorn {
-  constructor(name, color) {
-    this.name = name;
-    this.color = color;
+    constructor(name, color) {
+      this.name = name;
+      if (color === undefined) {
+        this.color = 'white';
+      } else {
+      this.color = color;
+    }
   }
+  isWhite() {
+    console.log(this.color);
+    if(this.color !== 'white') {
+      return false;
+    }
+  }
+  says(message) {
+    return `**;* ${message} *;**`;
+  }
+  //behavior
 }
 
 module.exports = Unicorn;
+
+// 1. read the test
+// 2. ask yourself if its going to fail and why
+// 3. run the test
+// 4. make it pass
