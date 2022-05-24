818dd4be1c4ee5ddb910a012e081e58d02c86afb
Dinne Kopelevich <dinne22@hotmail.com>
2022-05-23 20:29:09 -0600

    Add magician

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,17 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+  constructor() {
+    this.statues = [];
+  }
+  gazeAtVictim(victim) {
+    var aStatue = new Statue(victim.name);
+      this.name.push(victim.name);
+
+  }
+};
+
+
+
+module.exports = Medusa;
